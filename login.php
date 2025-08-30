c58696ce3ad441fcf29da21eeb21cd32ef9ba3f3
clair <clair.theshruyb@gmail.com>
2025-08-30 10:52:49 -0400

    added database connection, new mysqli object, and error handling if connectino fails  in login.php

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "admin";
+$password = "password";
+$dbname = "physicsclub";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn -> connect_error){
+    die("connected cailed: " . $conn -> connect_error);
+}
+
+?>
