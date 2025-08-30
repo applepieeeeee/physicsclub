010dc06e998eb23a9b220fb3e9a0a9aed61cde27
clair <clair.theshruyb@gmail.com>
2025-08-30 11:13:48 -0400

    fixed the spaces between ->

--- login.php
+++ login.php
@@ -8,25 +8,24 @@ $dbname = "physicsclub";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn -> connect_error){
-    die("connection failed: " . $conn -> connect_error);
+if ($conn->connect_error) {
+    die("connection failed: " . $conn->connect_error);
 }
 
 $input_username = $_POST['username'];
 $input_password = $_POST['password'];
 
 $sql = "SELECT username, password FROM admins WHERE username = ?";
-$stmt = $conn -> prepare($sql);
-$stmt -> bind_param("s", $input_username);
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $input_username);
 
-$stmt -> execute();
-$stmt -> store_result();
-$stmt -> bind_result($db_username, $db_password);
-$stmt -> fetch();
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($db_username, $db_password);
+$stmt->fetch();
 
-
-if ($stmt -> num_rows > 0){
-    if (password_verify($input_password, $db_password)){
+if ($stmt->num_rows > 0) {
+    if (password_verify($input_password, $db_password)) {
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $db_username;
         header("Location: admin_dashboard.php");
