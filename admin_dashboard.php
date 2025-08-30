233c9df8a59620492e597edc672f286a90f78904
clair <clair.theshruyb@gmail.com>
2025-08-30 11:05:27 -0400

    added admin_dashboard.php with just some basic text

---
+++ admin_dashboard.php
@@ -0,0 +1,14 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
+    header("Location: login.html");
+    exit;
+}
+
+echo "<h1> welcome to the admin dashboard!</h1>";
+echo "<p> you are logged in as " . htmlspecialchars($_SESSION['username']) . "</p>";
+echo '<a href = "logout.php">logout </a>';
+
+?>
