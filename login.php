d6100c66af1afc02b5db5e636c9c633fd52f4a97
clair <clair.theshruyb@gmail.com>
2025-08-30 10:55:14 -0400

    added sql query string, sql statment, and also line 19 bind user input to ? placeholder

--- login.php
+++ login.php
@@ -14,4 +14,8 @@ if ($conn -> connect_error){
 $input_username = $_POST['username'];
 $input_password = $_POST['password'];
 
+$sql = "SELECT username, password FROM admins WHERE username ="
+$stmt = $conn -> prepare($sql);
+$stmt -> bind_param("s", $input_username)''
+
 ?>
